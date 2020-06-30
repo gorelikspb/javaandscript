fa95d208f2b2b0fe4694958c30558e1b0936c47a
Your <you@example.com>
2020-06-30 15:06:42 +0300

    extracting perfAmount function

--- main.js
+++ main.js
@@ -36,27 +36,11 @@ function statement(invoice) {
             style: "currency", currency: "RUB",
             minimumFractionDigits: 2
         }).format;
-    let thisAmount = 0;
 
     for (let perf of invoice.performance) {
         // const play = plays[perf.playlD];
-        switch (perf.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`неизвестный тип: ${play.type}`);
-        }
+        const thisAmount = perfAmount(perf);
+
         // Добавление бонусов
         volumeCredits += Math.max(perf.audience - 30, 0);
         // Дополнительный бонус за каждые 10 зрителей
@@ -74,6 +58,31 @@ function statement(invoice) {
     // console.log(result);
     return result;
 }
+
+function perfAmount(perf) {
+    let thisAmount = 0;
+    switch (perf.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`неизвестный тип: ${play.type}`);
+    }
+
+    return thisAmount;
+}
+
+
 
 const result = statement(invoices[0]);
 
